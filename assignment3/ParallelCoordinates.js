05cd56c849077fc34be13aa57a0e0d179d5d3fc3
tordic <ctordi7545@gmail.com>
2018-10-08 15:44:46 -0500

    commenting and code cleaning for ParallelCoordinates.js

--- assignment3/ParallelCoordinates.js
+++ assignment3/ParallelCoordinates.js
@@ -1,91 +1,101 @@
+// Javascript file for Parallel Coordinates visualization
+// Authors: Eva Grench and Chris Tordi
+// Date: 10/7/18
+
+
 
 w = 1200;			// Width of our visualization
 h = 700;			// Height of our visualization
-margin = 10;
-xOffset = 40;
+margin = 10;        // Margin around visualization
+xOffset = 40;       // Space for x-axis labels
 transDur = 1500;	// Duration of transitions (in milliseconds)
-vals = ['flight_index','num_o_ring_distress','launch_temp','leak_check_pressure', 'tufte_metric']
+vals = ['flight_index','num_o_ring_distress','launch_temp','leak_check_pressure', 'tufte_metric'] // data columns
 
+// create svg element at parallelcoordinates id
 var svg = d3.select('#parallelcoordinates')
   .append('svg:svg')
   .attr('width', w)
   .attr('height', h + 12);
 
-function makeBar(yVal, data, yOffset) {
-  yScale = d3.scale.linear()
-  			.domain([d3.min(data, function(d) { return parseFloat(d[yVal]); })-1,
-  					 d3.max(data, function(d) { return parseFloat(d[yVal]); })+1])
-  			.range([h - margin, margin]); // Notice this is backwards!
+// create vertical bars along x-axis at barPosition
+function makeBar(yVal, data, barPosition) {
+    // scales y-axis for vertical bars
+    yScale = d3.scale.linear()
+    			.domain([d3.min(data, function(d) { return parseFloat(d[yVal]); })-1,
+    					 d3.max(data, function(d) { return parseFloat(d[yVal]); })+1])
+    			.range([h - margin, margin]);
 
-  yAxis = d3.svg.axis()
-				.scale(yScale)
-				.orient('left')
-				.ticks(3)
+    yAxis = d3.svg.axis()
+    			.scale(yScale)
+    			.orient('left')
+    			.ticks(3)
 
-
-  yAxisG = svg.append('g')
-				.attr('class', 'axis')
-				.attr('transform', 'translate(' + yOffset + ',0)')
+    yAxisG = svg.append('g')
+    			.attr('class', 'axis')
+    			.attr('transform', 'translate(' + barPosition + ',0)')
         .style({'stroke-width': '2px'})
-				.call(yAxis);
+    			.call(yAxis);
 
-  yLabel = svg.append('text')
-				.attr('class','label')
-				.attr('x', yOffset)
-				.attr('y', h + 10)
-				.text(yVal);
+    yLabel = svg.append('text')
+    			.attr('class','label')
+    			.attr('x', barPosition)
+    			.attr('y', h + 10)
+    			.text(yVal);
 }
 
-function drawLines(yValStart, yValEnd, yOffsetStart, yOffsetEnd, data) {
-  yScaleStart = d3.scale.linear()
-  			.domain([d3.min(data, function(d) { return parseFloat(d[yValStart]); })-1,
-  					 d3.max(data, function(d) { return parseFloat(d[yValStart]); })+1])
-  			.range([h - margin, margin]); // Notice this is backwards!
+// creates lines in between vertical bars
+function drawLines(yValStart, yValEnd, barPositionStart, barPositionEnd, data) {
+    yScaleStart = d3.scale.linear()
+    			    .domain([d3.min(data, function(d) { return parseFloat(d[yValStart]); })-1,
+    					 d3.max(data, function(d) { return parseFloat(d[yValStart]); })+1])
+    			    .range([h - margin, margin]);
 
-yScaleEnd = d3.scale.linear()
-			.domain([d3.min(data, function(d) { return parseFloat(d[yValEnd]); })-1,
-					 d3.max(data, function(d) { return parseFloat(d[yValEnd]); })+1])
-			.range([h - margin, margin]); // Notice this is backwards!
+    yScaleEnd = d3.scale.linear()
+    		      .domain([d3.min(data, function(d) { return parseFloat(d[yValEnd]); })-1,
+    				 d3.max(data, function(d) { return parseFloat(d[yValEnd]); })+1])
+    		      .range([h - margin, margin]);
 
-tooltip = d3.select('body').append('div')
-			.attr('class', 'tooltip')
-			.style('opacity', 0);
+    tooltip = d3.select('body').append('div')
+    		    .attr('class', 'tooltip')
+    		    .style('opacity', 0);
 
-  line = svg.selectAll('.line' + yValStart)
-    .data(data)
+    line = svg.selectAll('.line' + yValStart)
+              .data(data)
 
-  line.enter().append('line')
-      .attr('class', 'line' + yValStart)
-      .attr("x1", yOffsetStart)
-      .attr("y1", function(d) { return yScaleStart(d[yValStart]); })
-      .attr("x2", yOffsetEnd)
-      .attr("y2", function(d) { return yScaleEnd(d[yValEnd]); })
-      .style('stroke', 'SteelBlue')
-      .style('stroke-width', '2px')
-      .on("mouseover", function(d) {
-  			tooltip.transition()
-  				.duration(200)
-  				.style('opacity', .9);
-  			tooltip.html('<p> Flight Index <br>' + d["flight_index"] + '</p>')
-  				.style("left", (d3.event.pageX) + "px")
-  				.style("top", (d3.event.pageY - 28) + "px");
-        highlightLine(d, 'Crimson');
-      })
-      .on("mouseout", function(d) {
-  			tooltip.transition()
-  				.duration(400)
-  				.style('opacity', 0);
-        highlightLine(d, 'SteelBlue')
-  		})
-      .on("click", function(d) {
-        if (d3.select(this).style('stroke-width') == '2px') {
-          toggleLine(d, 'Crimson', '5px')
-        } else {
-          toggleLine(d, 'SteelBlue', '2px')
-        }
-      });
+    //attaches line data to each line
+    line.enter().append('line')
+        .attr('class', 'line' + yValStart)
+        .attr("x1", barPositionStart)
+        .attr("y1", function(d) { return yScaleStart(d[yValStart]); })
+        .attr("x2", barPositionEnd)
+        .attr("y2", function(d) { return yScaleEnd(d[yValEnd]); })
+        .style('stroke', 'SteelBlue')
+        .style('stroke-width', '2px')
+        .on("mouseover", function(d) {
+    	       tooltip.transition()
+    				  .duration(200)
+    				  .style('opacity', .9);
+               tooltip.html('<p> Flight Index <br>' + d["flight_index"] + '</p>')
+    				  .style("left", (d3.event.pageX) + "px")
+    				  .style("top", (d3.event.pageY - 28) + "px");
+               highlightLine(d, 'Crimson');
+        })
+        .on("mouseout", function(d) {
+              tooltip.transition()
+    				 .duration(400)
+    				 .style('opacity', 0);
+              highlightLine(d, 'SteelBlue')
+    		})
+        .on("click", function(d) {
+            if (d3.select(this).style('stroke-width') == '2px') {
+                toggleLine(d, 'Crimson', '5px')
+            } else {
+                toggleLine(d, 'SteelBlue', '2px')
+            }
+        });
 }
 
+// changes visual data of line when clicked
 function toggleLine(selectedLineData, newColor, newThickness) {
   lines = d3.selectAll('line')
   for (i = 0; i < lines.length; i++) {
@@ -98,6 +108,7 @@ function toggleLine(selectedLineData, newColor, newThickness) {
   }
 }
 
+// changes visual data of line when hovered over and moused out
 function highlightLine(selectedLineData, newColor) {
   lines = d3.selectAll('line')
 	for (i = 0; i < lines.length; i++) {
@@ -109,6 +120,7 @@ function highlightLine(selectedLineData, newColor) {
 	}
 }
 
+// loads csv data and calls create bars and create line functions
 d3.csv('challenger.csv', function(csvData) {
   data = csvData;
   for (i = 0; i < vals.length; i++) {
