a75be83ce595e6edcc54640b47e5d2fc5f3e1633
Ethan Cassel-Mace <casselmacee@carleton.edu>
2018-11-11 14:08:00 -0600

    aggregate some data

--- final-project/create_data.py
+++ final-project/create_data.py
@@ -18,9 +18,32 @@ required_fields = ['Gender',
                    'EthicsChoice',
                    'Exercise']
 
+value_mappings = {'Extremely satisfied': 'Satisfied',
+                  'Moderately satisfied': 'Satisfied',
+                  'Slightly satisfied': 'Neutral',
+                  'Slightly dissatisfied': 'Neutral',
+                  'Neither satisfied nor dissatisfied': 'Neutral',
+                  'Moderately dissatisfied': 'Dissatisfied',
+                  'Extremely dissatisfied': 'Dissatisfied'}
+
 
 def is_qualified(row):
     return row['Country'] == 'United States' and row['Student'] == 'No'
+
+
+def aggregate_data(field, value):
+    if field == 'Gender':
+        if value == 'Male' or value == 'Female':
+            return value
+        else: return 'Non-Binary'
+    if value in value_mappings:
+        return value_mappings[value]
+    else:
+        return value
+
+
+
+
 
 def create_new_row(row):
     new_row = []
@@ -28,7 +51,7 @@ def create_new_row(row):
     for field in required_fields:
         if row[field] == 'NA':
             return None
-        new_row.append(row[field])
+        new_row.append(aggregate_data(field, row[field]))
     return new_row
 
 # total_salary = 0
