3bb975c9ce599471396946e0330f0c351733e1de
Eva Grench <grenche@carleton.edu>
2018-11-11 13:47:34 -0600

    fixed the id.

--- final-project/final.js
+++ final-project/final.js
@@ -3,9 +3,9 @@
 // Date: 11/19/18
 
 var chart = d3.parsets()
-      .dimensions(["JobSatisfaction", "Gender", "CompanySize", "Hobby"]);
+      .dimensions(["JobSatisfaction", "Gender", "OpenSource", "Hobby"]);
 
-var vis = d3.select("#vis").append("svg")
+var vis = d3.select("#parsets").append("svg")
     .attr("width", chart.width())
     .attr("height", chart.height());
 
