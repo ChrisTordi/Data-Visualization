86b6a83bd4173a3aa28d6a26e0fff9745a4bf5ab
tordic <ctordi7545@gmail.com>
2018-11-13 22:10:59 -0600

    buttons!

--- final-project/static/final.js
+++ final-project/static/final.js
@@ -1,6 +1,7 @@
 // Javascript file for final project
 // Authors: Eva Grench, Ethan Cassel-Mace, Chris Tordi
 // Date: 11/19/18
+var attributesToShow = [];
 
 function constructVis(){
     var args = Array.prototype.slice.call(arguments);
@@ -14,5 +15,25 @@ function constructVis(){
     d3.csv("/static/output_data.csv", function(error, csv) {
       vis.datum(csv).call(chart);
     });
+}
 
+$(document).ready(function() {
+  $('.form-check-input').change(function() {
+      scanCheckBoxes($(this));
+  });
+});
+
+function scanCheckBoxes(attributeID) {
+    if (attributeID.is(":checked")) {
+        attributesToShow.push(attributeID.val());
+        $(".attribute-list").append("<p>hello</p>")
+    } else {
+        //remove from list
+        console.log(attributesToShow)
+        var index = attributesToShow.indexOf(attributeID.val());
+        if (index !== -1) array.splice(index, 1);
+
+        
+        // remove from html div
+        }
 }
